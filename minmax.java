8f56a4eaabe75110da996013cdc666de08b8bf2b
NAVATEJA <navatejar80@gmail.com>
2024-12-12 14:23:47 +0530

    Add files via upload

---
+++ minmax.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        public static void miniMaxsum(List<Integer>arr)
+        {
+            long mini,max,s=0;
+            Collections.sort(arr);
+            int n=arr.size();
+            for(int i=0;i<n;i++)
+            s+=arr.size(i);
+            System.out.println((s-arr.get(n-1))+" "(s-arr.get(o)));
+        }
+    }
+}
