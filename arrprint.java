b6026816a8c4803bad77f8ce8872291b2e67f5e1
NAVATEJA <navatejar80@gmail.com>
2024-12-12 12:20:09 +0530

    Add files via upload

---
+++ arrprint.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        // accepting arraylist elements
+        int n=sc.nextInt();
+        ArrayList<Integer>a=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int s=sc.nextInt();
+            a.add(s);
+        }
+        // printing the elements in an array list
+        for(int i=0;i<a.size();i++)
+        System.out.println(a.get(i)+" ");
+    }
+}
